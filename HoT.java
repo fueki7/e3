ea1aa98e5b572bd5bb45a3ce80e5b5819a41f6fc
s1310213 <s1310213@oflsmdc1.u-aizu.ac.jp>
2024-06-28 12:48:20 +0900

    HoT: initial revision

---
+++ HoT.java
@@ -0,0 +1,19 @@
+class HoT{
+    public static void main(String[] args){
+        int head=0;
+        int tail=0;
+        System.out.println("Tossing a coin...");
+        for(int i=1; i<=3; i++){
+            int d=(int)(Math.random()*2);
+            if(d==1){
+                head++;
+                System.out.println("Round "+i+": "+"Heads");
+            }
+            else if(d==0){
+                tail++;
+                System.out.println("Round "+i+": "+"Tails");
+            }
+        }
+        System.out.println("Heads: "+head+", Tails: "+tail);
+    }
+}
